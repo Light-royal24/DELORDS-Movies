586ecdfdd0521a561c5e4b3d822c8ff68bc90889
DELORD ROYAL <royalight24@gmail.com>
2023-06-20 07:41:53 -0700

    made changes on styling

--- main.js
+++ main.js
@@ -1,13 +1,71 @@
-document.addEventListener("DOMContentLoaded", async () => {
-  const getMoviesBtn = document.getElementById("movies-btn");
+let page = 1;
 
-  const upcomingMovies = await fetchMoviesLocal();
+// 1. Page loads
+document.addEventListener("DOMContentLoaded", async () => {
+  const movieUrl = "https://moviesdatabase.p.rapidapi.com/titles/x/upcoming";
+
+  // 2. Fetch upcoming movies and series
+  // const upcomingMovies = await fetchMoviesAPI(movieUrl);
+  const upcomingMovies = await fetchMoviesLocal(movieUrl);
+  // 3. Save the results of the fetch request in movies variable
   const movies = upcomingMovies.results;
 
+  // 4. Display the movies in our HTML
   displayMovies(movies);
+
+  // Saving our next button in variable
+  const nextPage = document.querySelector("#next");
+
+  // Listening for click event on our next button
+  nextPage.addEventListener("click", getNextPage);
+
+  // saving our previous button in variable
+  const previousBtn = document.querySelector("#Previous");
+
+  // listening for click event o previous page
+  previousBtn.addEventListener("click", getPreviousPage);
+
+  // get the search field
+  const search = document.getElementById("search");
+  search.addEventListener("keyup", async (e) => {
+    if (e.target.value.trim() === "") {
+      // 2. Fetch upcoming movies and series
+      // const upcomingMovies = await fetchMoviesAPI(movieUrl);
+      const upcomingMovies = await fetchMoviesLocal(movieUrl);
+      // 3. Save the results of the fetch request in movies variable
+      const movies = upcomingMovies.results;
+
+      // 4. Display the movies in our HTML
+      displayMovies(movies);
+    }
+  });
+
+  // add event listener to the search field
+  search.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const input = document.querySelector("input");
+    const inputValue = input.value;
+
+    if (inputValue.trim() !== "") {
+      try {
+        const searchPageURL = `https://moviesdatabase.p.rapidapi.com/titles/search/title/${inputValue}`;
+
+        // 2. Fetch upcoming movies and series
+        const upcomingMovies = await fetchMoviesAPI(searchPageURL);
+        // 3. Save the results of the fetch request in movies variable
+        const movies = upcomingMovies.results;
+
+        // 4. Display the movies in our HTML
+        displayMovies(movies);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  });
 });
 
-async function fetchMoviesAPI() {
+async function fetchMoviesAPI(movieUrl) {
+  toggleLoading(true);
   return new Promise((resolve, reject) => {
     var myHeaders = new Headers();
     myHeaders.append("X-RapidAPI-Key", apiKey);
@@ -18,15 +76,14 @@ async function fetchMoviesAPI() {
       redirect: "follow",
     };
 
-    fetch(
-      "https://moviesdatabase.p.rapidapi.com/titles/x/upcoming",
-      requestOptions
-    )
+    fetch(movieUrl, requestOptions)
       .then((response) => response.json())
       .then((result) => {
+        toggleLoading(false);
         resolve(result);
       })
       .catch((error) => {
+        console.log(error);
         reject(error);
       });
   });
@@ -71,14 +128,14 @@ function displayMovies(movies) {
 
       return `
 
-        <div class="movies" >
+        <div class="movie" >
           <div>
             <img src="${image}" />
           </div>
 
-          <h4 style="font-family: sans-serif; margin-top: 5px">
+          <h4 >
             ${title}
-          </h1>
+          </h4>
 
           
           <div class="dateTime">
@@ -88,11 +145,67 @@ function displayMovies(movies) {
   
         </div>
       `;
-    });
+    })
+    .join(" ");
 
   document.getElementById("movies").innerHTML = moviesHTML;
 }
 
-const nextPage = document.querySelector("#next");
+async function getNextPage() {
+  try {
+    page++;
+    const nextPageURL = `https://moviesdatabase.p.rapidapi.com/titles/x/upcoming?page=${page}`;
 
-nextPage.addEventListener("click", () => {});
+    // 2. Fetch upcoming movies and series
+    const upcomingMovies = await fetchMoviesAPI(nextPageURL);
+    // 3. Save the results of the fetch request in movies variable
+    const movies = upcomingMovies.results;
+
+    // 4. Display the movies in our HTML
+    displayMovies(movies);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function getPreviousPage() {
+  if (page == 1) {
+    return;
+  }
+
+  page--;
+
+  const previousPageUrl = `https://moviesdatabase.p.rapidapi.com/titles/x/upcoming?page=${page}`;
+
+  try {
+    if (page == 2) {
+      previousPageUrl ==
+        "https://moviesdatabase.p.rapidapi.com/titles/x/upcoming";
+    } else {
+      // 2. Fetch upcoming movies and series
+      const upcomingMovies = await fetchMoviesAPI(previousPageUrl);
+      // 3. Save the results of the fetch request in movies variable
+      const movies = upcomingMovies.results;
+
+      // 4. Display the movies in our HTML
+      displayMovies(movies);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function toggleLoading(loading) {
+  // Getting the overlay
+  let overlay = document.getElementsByClassName("loading-overlay")[0];
+
+  setTimeout(
+    () => {
+      // console.log(loading);
+
+      // Toggling is active on the overlay
+      overlay.classList.toggle("is-active");
+    },
+    loading ? 0 : 1500
+  );
+}
